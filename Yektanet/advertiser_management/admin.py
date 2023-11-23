1f5db29b190245d748871eca2277eefca238d2c1
Amin Khorasani <amin.a.khorasani@gmail.com>
2023-11-23 21:06:29 +0330

    Ad and Advertiser added to admin

--- Yektanet/advertiser_management/admin.py
+++ Yektanet/advertiser_management/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Ad, Advertiser
 
-# Register your models here.
+# register ad and advertiser class in admin
+admin.site.register(Ad)
+admin.site.register(Advertiser)
